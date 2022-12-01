8b11612267b689bea73a38e0c2615c1c7ffc7911
Ben Mordue <BMordue@hymans.co.uk>
2022-12-01 10:42:13 +0000

    Day 1 complete

--- 1/1.js
+++ 1/1.js
@@ -17,8 +17,9 @@ var __asyncValues = (this && this.__asyncValues) || function (o) {
 };
 const fs = require("fs");
 const readline = require("readline");
+const util = require("util");
 const lines = [];
-function processLineByLine() {
+function readLines() {
     var _a, e_1, _b, _c;
     return __awaiter(this, void 0, void 0, function* () {
         const fileStream = fs.createReadStream("input.txt");
@@ -51,9 +52,39 @@ function processLineByLine() {
         console.log(`read ${lines.length} lines`);
     });
 }
+function processLines() {
+    const elfTotals = [];
+    let elfIndex = 0;
+    elfTotals[0] = 0;
+    lines.forEach((line) => {
+        if (line) {
+            const calories = parseInt(line);
+            console.log(calories);
+            elfTotals[elfIndex] += calories;
+        }
+        else {
+            elfIndex++;
+            elfTotals[elfIndex] = 0;
+            console.log(`elf #${elfIndex}`);
+            console.log(`previous total: ${elfTotals[elfIndex - 1]}`);
+        }
+    });
+    console.log(`Highest calorie count is ${max(elfTotals)}`);
+    const sorted = sortDesc(elfTotals);
+    console.log(util.inspect(sorted));
+    const topThree = sorted[0] + sorted[1] + sorted[2];
+    console.log(`Sum of highest three calorie counts is ${topThree}`);
+}
+function sortDesc(arr) {
+    return arr.sort((a, b) => b - a);
+}
+function max(arr) {
+    return arr.reduce((a, b) => (a > b ? a : b));
+}
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        processLineByLine();
+        yield readLines();
+        processLines();
     });
 }
 main();
--- 1/1.ts
+++ 1/1.ts
@@ -1,9 +1,10 @@
 const fs = require("fs");
 const readline = require("readline");
+const util = require("util");
 
-const lines = [];
+const lines: string[] = [];
 
-async function processLineByLine() {
+async function readLines() {
   const fileStream = fs.createReadStream("input.txt");
 
   const rl = readline.createInterface({
@@ -19,8 +20,40 @@ async function processLineByLine() {
   console.log(`read ${lines.length} lines`);
 }
 
+function processLines() {
+  const elfTotals: number[] = [];
+  let elfIndex = 0;
+  elfTotals[0] = 0;
+  lines.forEach((line) => {
+    if (line) {
+      const calories = parseInt(line);
+      console.log(calories);
+      elfTotals[elfIndex] += calories;
+    } else {
+      elfIndex++;
+      elfTotals[elfIndex] = 0;
+      console.log(`elf #${elfIndex}`);
+      console.log(`previous total: ${elfTotals[elfIndex - 1]}`);
+    }
+  });
+  console.log(`Highest calorie count is ${max(elfTotals)}`);
+  const sorted = sortDesc(elfTotals);
+  console.log(util.inspect(sorted));
+  const topThree = sorted[0] + sorted[1] + sorted[2];
+  console.log(`Sum of highest three calorie counts is ${topThree}`);
+}
+
+function sortDesc(arr: number[]) {
+  return arr.sort((a, b) => b - a);
+}
+
+function max(arr: number[]) {
+  return arr.reduce((a, b) => (a > b ? a : b));
+}
+
 async function main() {
-  processLineByLine();
+  await readLines();
+  processLines();
 }
 
 main();
