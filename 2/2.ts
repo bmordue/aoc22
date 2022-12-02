ea24c63f2ef5d66364bd3d47ff62694ec2bbc9ee
Ben Mordue <BMordue@hymans.co.uk>
2022-12-02 11:37:24 +0000

    day 2 part 2

--- 2/2.ts
+++ 2/2.ts
@@ -1,10 +1,9 @@
 import { getLines } from '../aocutil';
  
-function processLines(lines :string[]) {
+function part1(lines :string[]) {
   let score = 0;
   
   for (let line of lines) {
-    console.log(line);
     let inc = 0;
     switch (line) {
       case 'A X': 
@@ -41,6 +40,31 @@ function processLines(lines :string[]) {
   console.log(`Total score is ${score}`);
 }
 
+
+function part2(lines :string[]) {
+  enum Them {'A', 'B', 'C'};
+  enum Us {'X', 'Y', 'Z'};
+  
+  const scores = [[3, 4, 8], [1, 5, 9] , [2, 6, 7]];
+
+  let score = sum(lines.map((line) => {
+    const tokens = line.split(' ');
+    const them = tokens[0] as keyof typeof Them;
+    const us = tokens[1] as keyof typeof Us;
+    let x = Them[them];
+    let y = Us[us];
+    
+    return scores[x][y];
+  }));
+
+  console.log(`Total score is ${score}`);
+
+}
+
+function sum(arr: number[]) {
+  return arr.reduce((a,b) => a + b);
+}
+
 function sortDesc(arr: number[]) {
   return arr.sort((a, b) => b - a);
 }
@@ -51,7 +75,7 @@ function max(arr: number[]) {
 
 async function main() {
   const lines = await getLines();
-  processLines(lines);
+  part2(lines);
 }
 
 main();
