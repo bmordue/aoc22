c076bb3f58fa8dd2d96bee7d3274959791c41e9f
Ben Mordue <BMordue@hymans.co.uk>
2022-12-13 16:45:37 +0000

    day 12 part 1 what a beast

--- 12/12.ts
+++ 12/12.ts
@@ -1,16 +1,21 @@
-import { dir } from "console";
+import { posix } from "path";
 import { start } from "repl";
-import { arrayBuffer } from "stream/consumers";
-import { inspect } from "util";
 import {
   addPos,
+  copyPos,
   equalPos,
   getLines,
   Position,
   posStr,
   subtractPos,
-  tail,
 } from "../aocutil";
+
+const directions: Position[] = [
+  { x: 0, y: 1 },
+  { x: 1, y: 0 },
+  { x: 0, y: -1 },
+  { x: -1, y: 0 },
+];
 
 function canClimb(grid: string[], current: Position, diff: Position): boolean {
   if (
@@ -30,17 +35,19 @@ function canClimb(grid: string[], current: Position, diff: Position): boolean {
 
   // console.log(`Checking height at ${posStr(diff)} from ${posStr(current)}`);
 
-  const newHeight: string = grid[current.y + diff.y][current.x + diff.x];
-
-  if (newHeight == "E") {
-    console.log("Reached the End!");
-    return true;
-  }
+  let newHeight: string = grid[current.y + diff.y][current.x + diff.x];
 
   const currentHeight: string = grid[current.y][current.x];
 
+  if (newHeight == "E") {
+    // console.log(
+    //   `Attempt climb to E from ${posStr(current)} (${currentHeight})`
+    // );
+    newHeight = "z";
+  }
+
   if (currentHeight == "S") {
-    console.log("Leaving the Start");
+    // console.log("Leaving the Start");
     return true;
   }
 
@@ -67,6 +74,83 @@ function arrayContains(arr: Position[], item: Position): boolean {
   return contains;
 }
 
+function pathsFrom(
+  grid: string[],
+  start: Position,
+  end: Position,
+  previousVisited: Position[],
+  solutions: Position[][]
+) {
+  let visited = previousVisited.map(copyPos);
+  visited.push(start);
+
+  // let limit = grid.length * grid[0].length;
+  // let allPaths: Position[][] = [];
+
+  let availableMoves = findAvailableMoves(start, visited, grid);
+
+  // let allRoutes: Position[][] = [];
+  // availableMoves.forEach((move) => {
+
+  for (let i = 0; i < availableMoves.length; i++) {
+    const move = availableMoves[i];
+    const newPos = addPos(start, move);
+    if (equalPos(newPos, end)) {
+      // visited.push(newPos);
+      // console.log(`Completed route! (${visited.length} steps)`);
+      // console.log(`${visited.map(posStr)}`);
+      solutions.push(visited);
+      // allRoutes.push(visited);
+      // allPaths.push(visited);
+    } else {
+      // allRoutes = allRoutes.concat(pathsFrom(grid, newPos, end, visited));
+      pathsFrom(grid, newPos, end, visited, solutions);
+    }
+  }
+
+  // return allRoutes;
+
+  // if (availableMoves.length == 0) {
+  //   console.log(`Stuck at ${posStr(start)}`);
+  //   //   iter = limit;
+  // }
+
+  // if (availableMoves.length > 0) {
+  //   start = addPos(start, availableMoves[0]);
+  // } else {
+  //   console.log(`Stuck at ${posStr(start)}`);
+  //   iter = limit;
+  // }
+}
+
+function findAvailableMoves(
+  curPos: Position,
+  path: Position[],
+  grid: string[]
+) {
+  let availableMoves: Position[] = [];
+  let candidates = directions
+    .map((dir) => {
+      return addPos(curPos, dir);
+    })
+    .filter((candidate: Position) => !arrayContains(path, candidate)); // don't revisit squares!
+
+  if (candidates.length > 3 && path.length > 1) {
+    console.log("Failed to prune the candidates:");
+    console.log(
+      `candidates: ${candidates.map(posStr)}; path: ${path.map(posStr)}`
+    );
+  }
+
+  for (let candidate of candidates) {
+    let dir = subtractPos(candidate, curPos); // *sigh*
+    if (canClimb(grid, curPos, dir)) {
+      availableMoves.push(dir);
+    }
+  }
+  return availableMoves;
+}
+
 function day12(this: any, grid: string[]) {
   const startY = grid.findIndex((row) => row.includes("S"));
   const startX = grid[startY].indexOf("S");
@@ -78,83 +162,98 @@ function day12(this: any, grid: string[]) {
 
   console.log(`S: ${posStr(start)} E: ${posStr(end)}`);
 
-  let curPos = start;
+  let solutions: Position[][] = [];
+  pathsFrom(grid, start, end, [], solutions);
 
-  let iter = 0;
-  let limit = grid.length * grid[0].length;
-  let path: Position[] = [];
+  console.log(`Found ${solutions.length} routes`);
+  // solutions.forEach((p) => console.log(`${p.map(posStr)} (${p.length})`));
 
-  while (!equalPos(curPos, end) && iter < limit) {
-    iter++;
-    path.push(curPos);
-    const directions: Position[] = [
-      { x: 0, y: 1 },
-      { x: 1, y: 0 },
-      { x: 0, y: -1 },
-      { x: -1, y: 0 },
-    ];
-    // const climbable = canClimb.bind(this, grid, curPos);
-    // let availableMoves: Position[] = directions.filter(climbable);
-    let availableMoves = [];
+  let minSteps = solutions
+    .map((p) => p.length)
+    .reduce((a, b) => (a > b ? b : a));
 
-    let candidates = directions
-      .map((dir) => {
-        return addPos(curPos, dir);
-      })
-      .filter((candidate: Position) => !arrayContains(path, candidate)); // don't revisit squares!
+  console.log(`Shortest route has ${minSteps} steps.`);
+}
 
-    // if (path.length > 1) {
-    // candidates =
-    // }
-    // candidates = candidates
-    // for (let dir of directions) {
-    //   if (canClimb(grid, curPos, dir)) {
-    //     availableMoves.push(dir);
-    //   }
-    // }
-    if (candidates.length > 3 && path.length > 1) {
-      console.log("Failed to prune the candidates:");
-      console.log(
-        `candidates: ${candidates.map(posStr)}; path: ${path.map(posStr)}`
-      );
+function gridAt(grid: number[][], pos: Position) {
+  return grid[pos.y][pos.x];
+}
+
+function day12try2(grid: string[]) {
+  const startY = grid.findIndex((row) => row.includes("S"));
+  const startX = grid[startY].indexOf("S");
+  const start = { x: startX, y: startY };
+
+  const endY = grid.findIndex((row) => row.includes("E"));
+  const endX = grid[endY].indexOf("E");
+  const end = { x: endX, y: endY };
+
+  console.log(`S: ${posStr(start)} E: ${posStr(end)}`);
+
+  let minSteps = 0;
+
+  let numGrid: number[][] = [];
+  //convert grid to numbers
+  for (let i = 0; i < grid.length; i++) {
+    const numRow = [];
+    for (let j = 0; j < grid[i].length; j++) {
+      numRow.push(grid[i].charCodeAt(j));
     }
-
-    for (let candidate of candidates) {
-      let dir = subtractPos(candidate, curPos); // *sigh*
-      if (canClimb(grid, curPos, dir)) {
-        availableMoves.push(dir);
-      }
-    }
-
-    if (availableMoves.length > 0) {
-      curPos = addPos(curPos, availableMoves[0]);
-    } else {
-      console.log(`Stuck at ${posStr(curPos)}`);
-      iter = limit;
-    }
-    // if (canClimb(grid, curPos, 0, 1)) {
-    //   console.log(`Move right from ${posStr(curPos)}`);
-    //   curPos = { x: curPos.x, y: curPos.y + 1 };
-    // } else if (canClimb(grid, curPos, 1, 0)) {
-    //   console.log(`Move up from ${posStr(curPos)}`);
-    //   curPos = { x: curPos.x + 1, y: curPos.y };
-    // } else if (canClimb(grid, curPos, 0, -1)) {
-    //   console.log(`Move left from ${posStr(curPos)}`);
-    //   curPos = { x: curPos.x, y: curPos.y - 1 };
-    // } else if (canClimb(grid, curPos, -1, 0)) {
-    //   console.log(`Move down from ${posStr(curPos)}`);
-    //   curPos = { x: curPos.x - 1, y: curPos.y };
-    // }
+    numGrid.push(numRow);
   }
 
-  let minSteps = path.length;
-  console.log(`Path taken: ${path.map(posStr)}`);
-  console.log(`Shortest route has ${minSteps} steps.`);
+  numGrid[start.y][start.x] = 97; //"a".charCodeAt(0);
+  numGrid[end.y][end.x] = "z".charCodeAt(0);
+
+  // console.log(numGrid);
+
+  let paths: Position[][] = [];
+  let visited: Position[] = [];
+
+  let frontier: { c: Position; p: Position[] }[] = [{ c: start, p: [] }];
+  let done = false;
+  while (frontier.length > 0 && !done) {
+    let current = frontier.shift();
+    let curPos = current?.c || { x: -1, y: -1 };
+    let curPath = current?.p || [];
+    let path = [...curPath];
+    path.push(curPos);
+    // let curPos = frontier.shift() || { x: -1, y: -1 };
+    // console.log(
+    //   `curPos: ${posStr(curPos)} | visited ${visited.map(
+    //     posStr
+    //   )} | frontier ${frontier.map(posStr)}`
+    // );
+    if (equalPos(curPos, end)) {
+      // done = true;
+      console.log(`Found a route with ${path.length - 1} steps.`);
+      // console.log(`${visited.map(posStr)}`);
+    }
+    if (!arrayContains(visited, curPos)) {
+      let currentHeight = gridAt(numGrid, curPos);
+      directions.forEach((dir) => {
+        let candidate = addPos(curPos, dir);
+        if (
+          candidate.x >= 0 &&
+          candidate.x < numGrid[0].length &&
+          candidate.y >= 0 &&
+          candidate.y < numGrid.length
+        ) {
+          if (gridAt(numGrid, candidate) <= currentHeight + 1) {
+            frontier.push({ c: candidate, p: path });
+          }
+        }
+      });
+      visited.push(curPos);
+    }
+  }
+
+  //console.log(`Shortest route has ${minSteps} steps.`);
 }
 
 async function main() {
   const lines = await getLines();
-  day12(lines);
+  day12try2(lines);
 }
 
 main();
