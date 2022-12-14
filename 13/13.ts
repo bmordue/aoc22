318a8911040fe284957e3af7e80a4144152007b9
Ben Mordue <BMordue@hymans.co.uk>
2022-12-14 15:50:04 +0000

    day 13 dodgy start

--- 13/13.ts
+++ 13/13.ts
@@ -1,6 +1,10 @@
-import { parse } from "path";
-import { createSecureContext } from "tls";
+import { inspect } from "util";
 import { getLines, sum } from "../aocutil";
+
+interface TreeNode {
+  parent: TreeNode | null;
+  children: TreeNode[];
+}
 
 async function main() {
   const lines = await getLines();
@@ -18,10 +22,15 @@ function day13(lines: string[]) {
     // read a pair
     const left = lines.shift() || "";
     const right = lines.shift() || "";
-    const leftList = parseList(left);
-    const rightList = parseList(right);
+    // const leftList = parseList(left);
+    // const rightList = parseList(right);
 
-    console.log(`${leftList} | ${rightList}`);
+    const allTokensL: any[] | undefined = [];
+    const allTokensR: any[] | undefined = [];
+    const leftTree = tokenise(left, allTokensL);
+    const rightTree = tokenise(right, allTokensR);
+
+    console.log(`${inspect(allTokensL)} | ${inspect(allTokensR)}`);
 
     // discard blank line
     lines.shift();
@@ -34,62 +43,68 @@ function day13(lines: string[]) {
   console.log(`Sum of indices is ${sumIndices}`);
 }
 
-function parseList(line: string): any[] {
-  console.log(`Try to parse: ${line}`);
-  let result = [];
-  line = line.substring(1, line.length);
-  let tokens = line.split(',');
-  for (let i = 0; i < line.length; i++) {
-    let char = line[i];
+function buildTree(root: TreeNode, line: string): TreeNode {
+  //   console.log(`Try to parse: ${line}`);
+  let tree: TreeNode = { parent: root, children: [] };
+  line = line.substring(1, line.length); // line will always be a list
 
-    if (char == "[") {
-      let j = i;
-      let next;
-      let openBrackets = 1;
-      while (openBrackets > 0) {
-        j++;
-        next = line[j];
-        if (next == "[") {
-          openBrackets++;
-        } else if (next == "]") {
-          openBrackets--;
-        }
-      }
-      const listStr = line.substring(i + 1, j);
-      console.log(`listStr: ${listStr}`);
-      result.push(parseList(listStr));
-      i = j;
-    }
-  }
-  return result;
+  //   for (let i = 0; i < line.length; i++) {
+  //     let char = line[i];
+
+  //     if (char == "[") {
+  //       let j = i;
+  //       let next;
+  //       let openBrackets = 1;
+  //       while (openBrackets > 0) {
+  //         j++;
+  //         next = line[j];
+  //         if (next == "[") {
+  //           openBrackets++;
+  //         } else if (next == "]") {
+  //           openBrackets--;
+  //         }
+  //       }
+  //       const listStr = line.substring(i + 1, j);
+  //       //   console.log(`listStr: ${listStr}`);
+  //       tree.children.push(buildTree(tree, listStr));
+  //       i = j;
+  //     } else if (char == ",") {
+  //       let j = i;
+  //       let next;
+  //       while (next != ",") {
+  //         j++;
+  //         next = line[j];
+  //       }
+  //       tree.children.push;
+  //       i = j;
+  //     }
+  //   }
+  return tree;
 }
 
-function parseListBorked(line: string) {
-  // strip brackets
-  let contents = line.substring(1, line.length - 1);
-
-  //tokenize on comma
-  let tokens = contents.split(",");
-
-  let result: any[] = [];
-  while (tokens) {
-    let token = tokens.shift() || "";
-    if (token[0] == "[") {
-      // const close = tokens.findIndex((t) => t == );
-
-      result.push(parseList(token));
+function tokenise(line: string, allTokens: any[] = []) {
+  // line will always be a list, skip outermost brackets
+  let tokens: any[] = [];
+  let openBrackets = 0;
+  let currentToken = "";
+  for (let i = 1; i < line.length - 1; i++) {
+    if (line[i] == "[") {
+      openBrackets++;
+    } else if (line[i] == "," && currentToken != "" && openBrackets == 0) {
+      tokens.push(currentToken);
+      currentToken = "";
+    } else if (line[i] == "]") {
+      openBrackets--;
+      if (openBrackets == 0) {
+        tokens.push(tokenise(currentToken));
+      }
     } else {
-      let num = parseInt(token);
-      if (!isNaN(num)) {
-        result.push(num);
-      }
+      currentToken += line[i];
     }
   }
-
-  // parse tokens
-  console.log(`parsed "${line}" as "${tokens}"`);
-  return result;
+  allTokens.push(tokens);
 }
+
 function correctlyOrdered(leftList: any[], rightList: any[]) {
   return false;
 }
